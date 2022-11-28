7653a1f0a5a30ab0c7c2b1f4dea83e954acbe33f
Alexander Laevens <alaevens@ualberta.ca>
2022-11-27 20:34:23 -0700

    manage.py now respects settings mode

--- one_trip_api/manage.py
+++ one_trip_api/manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'one_trip_api.settings.dev')
+    settings = 'one_trip_api.settings.dev'
+    if os.getenv("DJANGO_RELEASE", False):
+        settings = 'one_trip_api.settings.release'
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
